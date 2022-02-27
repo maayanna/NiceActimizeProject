de7c6da447d7d4f6e8087b5830ebcc761035d3bf
maayanna <maayann.affriat@gmail.com>
2022-02-27 09:13:52 +0200

    Passing all methods as static, no instance needed for every file.

--- src/main/java/DAUCounter.java
+++ src/main/java/DAUCounter.java
@@ -5,16 +5,12 @@ import java.util.HashSet;
 public class DAUCounter
 {
 
-    private String fileName;
-    private HashMap<String, HashSet<String>> myMap;
-
-
-    DAUCounter(String fileName){
-        this.fileName = fileName;
-        this.myMap = new HashMap<>();
-    }
-
-    private void parseFile(){
+    /**
+     * This function is opening the file and parsing it into a hashmap.
+     * The key is the date, and the value is a hashset with the users of that day.
+     * Like that we pass only once through the lines of the file.
+     */
+    private static void parseFile(String fileName, HashMap<String, HashSet<String>> myMap){
         try{
 
             FileReader fileReader = new FileReader(fileName);
@@ -22,14 +18,14 @@ public class DAUCounter
             String line = reader.readLine();
             String[] values;
 
-            while(line != null){
+            while(line != null){  // Line by line
 
-                values = line.split(",");
+                values = line.split(",");  // Separate the line to find the userId and the date he logged in
                 String userId = values[0];
                 String date = values[1].split(" ")[0];
 
                 HashSet<String> mySet;
-                if (myMap.containsKey(date)){ // Already has a date
+                if (myMap.containsKey(date)){ // Already has a date and has then already a hashset
                     mySet = myMap.get(date);
                 }
 
@@ -45,11 +41,36 @@ public class DAUCounter
 
         }
         catch (IOException e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.getMessage()); // couldn't open the file or find it either
         }
+    }
+
+    /**
+     * This function prints the number of DAU day by day, according to the hashmap we filled
+     * while parsing the file.
+     */
+    private static void printDAUByDay(HashMap<String, HashSet<String>> myMap){
+        for (String str : myMap.keySet()){
+            System.out.println("the DAU for " + str + " is " + myMap.get(str).size());
+        }
+    }
+
+    /**
+     * This function manages the other functions and runs the program from start to end.
+     * @param fileName - the file to parse in order to find the DAU users od a day.
+     */
+    private static void runAll(String fileName){
+        HashMap<String, HashSet<String>> myMap = new HashMap<>();
+        parseFile(fileName, myMap);
+        printDAUByDay(myMap);
     }
 
     public static void main(String[] args) {
 
+        if (args.length != 1){
+            System.err.println("You need to enter only a file path as an argument !");
+            return;
+        }
+        runAll(args[0]); // First arg is supposed to be the fileName
     }
 }
--- src/test/java/DAUCounterTest.java
+++ src/test/java/DAUCounterTest.java
@@ -1,2 +1,18 @@
+import org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class DAUCounterTest {
+
+    @Test
+    public void testApp(){
+        assert true;
+    }
+
+    public void testParseFile(String fileName, HashMap<String, HashSet<String>> myMap){
+        HashMap<String, HashSet<String>> parsingMap = 
+    }
+
 }
